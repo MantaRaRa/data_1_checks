9ecea534224af488ce9c2058561323bb44053587
Von VanMany <vonvanmany@gmail.com>
2022-09-14 23:54:53 -0400

    Knowledge Test

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello,World")
+
+list = ["make bed", "finish Drywall", "Brad Nail Base Boards", "Mud seams"]
+print(list[2])
+
+dict = {
+    "color": "Teal",
+    "food": "Pho",
+    "year": 2000,
+}
+print(dict['color'])
+
+tuple = (100, 200, 300, 400, 500)
+print(tuple[0])
